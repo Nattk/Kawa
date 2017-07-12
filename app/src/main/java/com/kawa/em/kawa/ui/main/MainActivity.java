1ba476dd39c9dddaebc6ffcf38bedf6ba4361778
Nattk <Kifoyinattan@gmail.com>
2017-07-12 14:46:10 +0200

    add constant and splash timer

--- app/src/main/java/com/kawa/em/kawa/ui/main/MainActivity.java
+++ app/src/main/java/com/kawa/em/kawa/ui/main/MainActivity.java
@@ -1,15 +1,33 @@
 package com.kawa.em.kawa.ui.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.kawa.em.kawa.R;
+import com.kawa.em.kawa.ui.map.MapActivity;
+import com.kawa.em.kawa.utils.Constant;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Timer myTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+
+                    Intent intentMap = new Intent(MainActivity.this, MapActivity.class);
+                    startActivity(intentMap);
+            }
+        }, Constant.SPLASH_TIME);
     }
 }
---
+++ app/src/main/java/com/kawa/em/kawa/utils/Constant.java
@@ -0,0 +1,11 @@
+package com.kawa.em.kawa.utils;
+
+/**
+ * Created by Nattan on 12/07/2017.
+ */
+
+public class Constant {
+
+    public static final long SPLASH_TIME = 2000;
+
+}
