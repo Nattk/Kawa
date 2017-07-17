aee205b4112195dae2f89ee5b0e5cae6fa854578
Nattk <Kifoyinattan@gmail.com>
2017-07-17 11:24:11 +0200

    erreur listfragment

--- app/src/main/java/com/kawa/em/kawa/ui/ListeCafe/ListFragment.java
+++ app/src/main/java/com/kawa/em/kawa/ui/ListeCafe/ListFragment.java
@@ -2,16 +2,14 @@ package com.kawa.em.kawa.ui.ListeCafe;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.kawa.em.kawa.R;
+import com.kawa.em.kawa.models.Cafes.Cafe;
 import com.kawa.em.kawa.models.Cafes.Cafes;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,12 +17,12 @@ import java.util.List;
 public class ListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final List<Cafes> cafes = new ArrayList<>();
-    private String TAG = "FragmentList";
-
+    private static final List<Cafes> cafes = Collections.emptyList();
+    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private Serializable mParam1;
+    private List mParam1;
+    private String mParam2;
 
     public ListFragment() {
         // Required empty public constructor
@@ -35,13 +33,15 @@ public class ListFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
      * @return A new instance of fragment ListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ListFragment newInstance(List<Cafes> param1) {
+    public static ListFragment newInstance(param1, List<Cafe> param2) {
         ListFragment fragment = new ListFragment();
         Bundle args = new Bundle();
-        args.putSerializable("listCafes", (Serializable) param1);
+        args.putSerializable("listCafes", param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,9 +50,8 @@ public class ListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getSerializable("listCafes");
-
-            Log.e(TAG,"Arguments : "+mParam1);
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
